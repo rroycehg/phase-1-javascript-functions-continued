b607f46c79fd3f2c8502b0acd2b861e3b1a835bd
Roli <rroycehg@gmail.com>
2021-05-21 22:34:30 -0400

    commit tree ac9ee5e48f1f72984dbc6209c1499d1122066de8

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+function saturdayFun(activity){
+    let regular = "roller-skate";
+    if (activity != null){
+        return `This Saturday, I want to ${activity}!`;
+    }  
+        else  {
+        return `This Saturday, I want to ${regular}!`;
+    }
+}
+
+//Tried different approaches using If()
+
+function mondayWork (officeact) {
+    let regular = "go to the office";
+    if (officeact){
+        return `This Monday, I will ${officeact}.`;
+        
+    } else {
+        return `This Monday, I will ${regular}.`;
+    }
+    
+}
+
+
+ 
+
+     let wrapAdjective = function(highlight ="*") {
+        return function(adjective ="special") {
+          return `You are ${highlight}${adjective}${highlight}!`
+        }
+      }
